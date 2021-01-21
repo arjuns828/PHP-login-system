2acf63bd5d5494ea036b1fd240e93d3e26d66174
arjuns828 <arjuns828@gmail.com>
2021-01-22 00:01:48 +0530

    updated files

--- Inc/config.php
+++ Inc/config.php
@@ -3,4 +3,10 @@
     if(!defined('__CONFIG__')){
         exit('you do not have the config file');
     }
+
+
+    // include the DataBase file
+    include_once "classess/DataBase.php";
+
+    $con=DataBase::getConnection();
 ?>
--- Inc/footer.php
+++ Inc/footer.php
@@ -4,3 +4,5 @@
 <!-- UIkit JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+
+<script src="php_login_course/PHP-login-system/assets/main.js"></script>
