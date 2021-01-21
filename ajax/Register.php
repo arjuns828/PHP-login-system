c684c6f5d4ff641b95357036046c0347fb8bd75e
arjuns828 <arjuns828@gmail.com>
2021-01-22 00:36:00 +0530

    ajax

---
+++ ajax/Register.php
@@ -0,0 +1,26 @@
+<?php 
+
+	// Allow the config
+//	define('__CONFIG__', true);
+
+	// Require the config
+//	require_once "../Inc/config.php"; 
+
+//	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+//		//  return JSON format
+//		header('Content-Type: application/json');
+
+//		$return = [];
+//
+        //user must be existed does not exist. 
+  //      $
+
+	//	$return['redirect'] = '';
+	//	$return['name'] = "Arjun Sharma";
+
+	//	echo json_encode($return, JSON_PRETTY_PRINT); exit;
+	//} else {
+
+	//	exit('test');
+	//}
+?>
